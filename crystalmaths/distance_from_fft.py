64eb8d77bdb2595cc49068be001f5d622fc8fd79
edwinj5 <56658990+edwinj5@users.noreply.github.com>
2020-03-10 18:22:53 -0700

    Measure distance of two points of fft image

---
+++ crystalmaths/distance_from_fft.py
@@ -0,0 +1,13 @@
+import math
+import pandas as pd
+import numpy as np
+
+def dis_cal(point, ratio):
+    '''This is a function used to calculate the distance between one point and origin. The ratio should be in unit of pixel/nm'''
+    cord_list = []
+    for i in range (len(point)):
+        cord_list.append(point[i]**2)
+    dis_calculation = ratio/math.sqrt(sum(cord_list))
+    return dis_calculation
+
+
