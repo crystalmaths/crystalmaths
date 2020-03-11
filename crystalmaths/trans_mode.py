8e1077ac5d5c0423a2138184ced30ce8c2de77a7
edwinj5 <56658990+edwinj5@users.noreply.github.com>
2020-03-10 18:22:11 -0700

    a function used to translate angle from rad to degree or four parameters to three

---
+++ crystalmaths/trans_mode.py
@@ -0,0 +1,23 @@
+
+import math
+import numpy as np
+
+def hexag_trans(vector):
+    '''This is a function used to translate hexagonal lattice with four coordinates to three coordinates. Vector should be written in numpy.array format with four parameters'''
+    after = np.zeros(3)
+    after[0] = vector[0]-vector[2]
+    after[1] = vector[1]-vector[2]
+    after[2] = vector[3]
+    return after
+
+def angle_to_degree(alpha1, beta1, gamma1):
+    '''This is a function used to translate angle from rad to degree, 
+    user can obtain degree directly from the function'''
+    alpha = alpha1*180/math.pi
+    beta = beta1*180/math.pi
+    gamma = gamma1*180/math.pi
+    print(alpha)
+    print(beta)
+    print(gamma)
+    return alpha, beta, gamma
+
