18ac77ca8c42b285f54ce52d1305e15c737c183d
Kacper Lachowski <lachowskik27@gmail.com>
2020-03-11 02:39:51 -0700

    ImageHandler class now prompts user to select features on image and creates attribute called point_coordinates.

--- crystalmaths/imagetools.py
+++ crystalmaths/imagetools.py
@@ -61,7 +61,7 @@ class ImageHandler():
         self.image_filepath = image_filepath
         self.image_array = skimage.io.imread(self.image_filepath)
         image_fft = np.abs(np.fft.fftshift((np.fft.fft2(self.image_array))))
-        self.image_fft = np.log(image_fft)
+        self.image_fft_array = np.log(image_fft)
 
     def show_image(self):
         fig, axes = plt.subplots(1, 2, figsize=(16, 16))
@@ -72,16 +72,39 @@ class ImageHandler():
         plt.axis('off')
 
     def get_planes(self):
-        prompt = 'Pick planes using left mouse button. Press a key to \
-            terminate. Press right mouse button to restart.'
+        prompt = 'Pick planes using left mouse button. Right click once done.\
+ Middle mouse button removes most recent point.'
+        message = "Press keyboard button to save points, mouseclick to restart"
         print(prompt)
         fig, ax = plt.subplots()
-        plt.title(prompt, fontsize=16)
-        plt.setp(plt.gca(), autoscale_on=False)
+        plt.setp(plt.gca(), autoscale_on=True)
         ax.imshow(self.image_fft, cmap='binary_r')
-        pts = []
+        # zoom = False
+        # while zoom is not True:
         while True:
-            clicked_on = plt.ginput()
-            print(clicked_on)
-            # pts.append(plt.ginput())
-            # if
+            plt.title('Perform zoom if necessary, keyboard press to advance')
+            if plt.waitforbuttonpress(timeout=-1):
+                # zoom = True
+                break
+        print("Exited zoom loop")
+        while True:
+            plt.title(prompt, wrap=True)
+            fig.canvas.draw()
+            while True:
+                print("Entered point pick loop")
+                points = plt.ginput(n=-1, show_clicks=True, timeout=-1,
+                                    mouse_add=1, mouse_stop=3, mouse_pop=2)
+                break
+            plt.title(message, wrap=True)
+            fig.canvas.draw()
+            print("Saved points = ", points)
+            print(message)
+            point_list = []
+            if plt.waitforbuttonpress():
+                for i in range(len(points)):
+                    point_col = int(points[i][0])
+                    point_row = int(points[i][1])
+                    point_list.append([point_col, point_row])
+                self.point_coordinates = point_list
+                plt.close()
+                break
