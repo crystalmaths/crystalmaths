d4255fdce15acb71ff1948cb9dd0a7cffbf17a07
Edwin Jiang <edwinj5@uw.edu>
2020-03-12 19:38:31 -0700

    unit test for test_distance_from_fft.py after passing flake8

--- crystalmaths/test_distance_from_fft.py
+++ crystalmaths/test_distance_from_fft.py
@@ -1,9 +1,9 @@
 import distance_from_fft
 
+
 def test_dis_cal():
     point = [1, 2]
     ratio = 2
     result1 = distance_from_fft.dis_cal(point, ratio)
     assert result1 != float, "The calculated result is in wrong type"
-    assert round(result1, 3) == 0.894, "The calcualtion is incorrect" 
-    
+    assert round(result1, 3) == 0.894, "The calcualtion is incorrect"
