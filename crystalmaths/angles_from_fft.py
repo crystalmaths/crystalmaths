2e3988c609a6a877e1aad45f73bf1dd4bc1d5d2c
Edwin Jiang <edwinj5@uw.edu>
2020-03-12 19:41:24 -0700

    angles_from_fft.py file after flake8 testing

--- crystalmaths/angles_from_fft.py
+++ crystalmaths/angles_from_fft.py
@@ -1,31 +1,36 @@
 import math
-import pandas as pd
-import numpy as np
+
 
 def dotproduct(point1, point2):
-    """This is the function used to calculate the dot product between two vectors, which will be used as numerator for angle calculation"""
+    """This is the function used to calculate the dot \
+    product between two vectors, which will be used as \
+    numerator for angle calculation"""
     result = 0
-    for i in range (len(point1)):
+    for i in range(len(point1)):
         result = result + point1[i]*point2[i]
     return result
 
+
 def lengthproduct(point1, point2):
-    """This is the function used to calculate the length of given two vectors and their product, which will be used as denominator for angle calculation"""
+    """This is the function used to calculate the length \
+    of given two vectors and their product, which will be \
+    used as denominator for angle calculation"""
     len1 = 0
     len2 = 0
-    for i in range (len(point1)):
+    for i in range(len(point1)):
         len1 = len1 + point1[i]*point1[i]
-    for i in range (len(point2)):
+    for i in range(len(point2)):
         len2 = len2 + point2[i]*point2[i]
     len1 = math.sqrt(len1)
     len2 = math.sqrt(len2)
     lengthproduct = len1*len2
     return lengthproduct
 
-def angle (point1, point2):
-    """This is the function used to calculate the angle between two vectors, by calculating arccosine of dot product divide by length product"""
+
+def angle(point1, point2):
+    """This is the function used to calculate the angle \
+    between two vectors, by calculating arccosine of dot \
+    product divide by length product"""
     rad = math.acos(dotproduct(point1, point2)/lengthproduct(point1, point2))
     degree = 180*rad/math.pi
     return degree
-
-
