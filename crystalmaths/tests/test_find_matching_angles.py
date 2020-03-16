3283ffdb4f675150892d03a1408ca9f2239b0985
Elena Shoushpanova <eshoush@uw.edu>
2020-03-15 17:59:26 -0700

    Updated the test function for matching angles, due to new input style.

--- crystalmaths/tests/test_find_matching_angles.py
+++ crystalmaths/tests/test_find_matching_angles.py
@@ -1,12 +1,46 @@
 import find_matching_angles
+import numpy as np
+import pandas as pd
 
 
 def test_find_matching_angles():
-    all_angles_fft = [11, 29, 78]
-    all_angles_data = [10, 30, 50, 80]
+    """
+    This is a test function for "find_matching_angles" function.
+    """
+
+    # Creating a "fake" angle from FFT:
+    angle_fft = 35
+
+    # Creating a "fake" data table to simulate results from
+    # "AllAnglePairs", which has angles ffrom structure data base and
+    # corresponding pairs of HKL planes:
+    H1 = np.array([1, 1, 1])
+    K1 = np.array([0, 1, 1])
+    L1 = np.array([0, 0, 1])
+    H2 = np.array([1, 1, 1])
+    K2 = np.array([1, 1, 0])
+    L2 = np.array([0, 1, 0])
+    angles = np.array([45, 35.264, 54.736])
+    angles_sd_df = pd.DataFrame()
+    angles_sd_df['H1'] = H1
+    angles_sd_df['K1'] = K1
+    angles_sd_df['L1'] = L1
+    angles_sd_df['H2'] = H2
+    angles_sd_df['K2'] = K2
+    angles_sd_df['L2'] = L2
+    angles_sd_df['angle'] = angles
+
+    # Specifying tolerance:
     tolerance = 1
-    matching_angles = find_matching_angles(all_angles_fft, all_angles_data,
-                                           tolerance)
-    for i in range(0, len(matching_angles)):
-        assert abs(matching_angles[i][0] - matching_angles[i][1]) <=\
-            tolerance, "Wrong pair of angles"
+
+    # Calling for a function
+    final_results = find_matching_angles.find_matching_angles(angle_fft,
+                                                              angles_sd_df,
+                                                              tolerance)
+
+    for i in range(0, final_results.shape[0]):
+        if abs(final_results['angle'][i] -
+               final_results['angle-fft'][i]) <= tolerance:
+            assert final_results['angle match'][i] == 1, "Wrong matching"
+        else:
+            assert final_results['angle match'][i] == 0, "Wrong matching"
