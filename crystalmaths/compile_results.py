4a454261e325dfc7941ddc13900e5666cb9d9959
Kacper Lachowski <lachowskik27@gmail.com>
2020-03-16 23:06:39 -0700

    Fixed bug for compile_results.

--- crystalmaths/compile_results.py
+++ crystalmaths/compile_results.py
@@ -38,13 +38,15 @@ def compile_results(image_object, d_spacing_tolerance, angle_tolerance,
         result_df = temp_object.result_df
         result_df = crystalmaths.find_matching_angles.find_matching_angles(
             fft_angle, result_df, angle_tolerance)
-        if final_df.empty:
-            print("No results identified from search.")
-        else:
+
+        try:
             final_df = result_df[result_df['angle match'] is True]
-            mineral_name = metadata_df['Mineral_Name'].values
-            n = final_df.shape[0]
-            mineral_name_list = [mineral_name for i in range(n)]
-            final_df.insert(0, 'Mineral_Name', mineral_name_list)
-            angle_compare_list.append((final_df, metadata_df))
+        except Exception:
+            print("No results identified from search.")
+            break
+        mineral_name = metadata_df['Mineral_Name'].values
+        n = final_df.shape[0]
+        mineral_name_list = [mineral_name for i in range(n)]
+        final_df.insert(0, 'Mineral_Name', mineral_name_list)
+        angle_compare_list.append((final_df, metadata_df))
     return angle_compare_list
