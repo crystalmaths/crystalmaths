73df04b5a2f60834872f28a93d60a9e30aaf35c6
Elena Shoushpanova <eshoush@uw.edu>
2020-03-19 17:30:01 -0700

    Updated for PEP8, also added "" where it was missing

--- crystalmaths/setup.py
+++ crystalmaths/setup.py
@@ -4,14 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="AmyStegmann", 
+    name="AmyStegmann",
     version="0.0.1",
     author="Example Author",
     author_email="amybrehm@uw.edu",
     description="Identify zone axis from a high res TEM image",
-    long_description=Crystalmaths is a Data Science Project that was developed as a part of the DIRECT Program by a group of graduate students from the University of Washington in Seattle.
-The goal of crystalmaths is to create a Python module that can make automate zone axis definition from a high resolution TEM images.
-crystalmaths is a developing module with more improvements to come. Feel free to add your comments, notes, and recommendations in the issues of this repository.,
+    long_description="Crystalmaths is a Data Science Project that was\
+    developed as a part of the DIRECT Program by a group of graduate students\
+    from the University of Washington in Seattle.\
+    crystalmaths is a developing module with more improvements to come.\
+    Feel free to add your comments, notes, and recommendations in the issues\
+    of this repository.",
     long_description_content_type="text/markdown",
     url="https://github.com/crystalmaths/crystalmaths",
     packages=setuptools.find_packages(),
